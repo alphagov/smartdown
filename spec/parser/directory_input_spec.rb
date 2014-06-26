426f0f36dbc2679f77d89fa83fc218b07f7882c4
David Heath <david.heath@digital.cabinet-office.gov.uk>
2014-06-26 14:06:06 +0100

    Add DirectoryInput to abstract reading files from filesystem

---
+++ spec/parser/directory_input_spec.rb
@@ -0,0 +1,56 @@
+require 'smartdown/parser/directory_input'
+
+shared_examples "flow input interface" do
+  it { should respond_to(:coversheet) }
+  it { should respond_to(:questions) }
+  it { should respond_to(:outcomes) }
+  it { should respond_to(:scenarios) }
+end
+
+describe Smartdown::Parser::DirectoryInput do
+  it_should_behave_like "flow input interface"
+
+  let(:coversheet_file) {
+    Pathname.new("../../fixtures/directory_input/cover-sheet.txt").expand_path(__FILE__)
+  }
+
+  subject(:input) { described_class.new(coversheet_file) }
+
+  describe "#coversheet" do
+    subject { input.coversheet }
+
+    it { should be_a(Smartdown::Parser::InputFile) }
+
+    it "has name" do
+      expect(input.coversheet.name).to eq("cover-sheet")
+    end
+
+    it "reads the file contents" do
+      expect(input.coversheet.read).to eq("cover sheet\n")
+    end
+  end
+
+  describe "#questions" do
+    it "returns an InputFile for every file in the questions folder" do
+      expect(input.questions).to match([instance_of(Smartdown::Parser::InputFile)])
+      expect(input.questions.first.name).to eq("q1")
+      expect(input.questions.first.read).to eq("question one\n")
+    end
+  end
+
+  describe "#outcomes" do
+    it "returns an InputFile for every file in the outcomes folder" do
+      expect(input.outcomes).to match([instance_of(Smartdown::Parser::InputFile)])
+      expect(input.outcomes.first.name).to eq("o1")
+      expect(input.outcomes.first.read).to eq("outcome one\n")
+    end
+  end
+
+  describe "#scenarios" do
+    it "returns an InputFile for every file in the scenarios folder" do
+      expect(input.scenarios).to match([instance_of(Smartdown::Parser::InputFile)])
+      expect(input.scenarios.first.name).to eq("s1")
+      expect(input.scenarios.first.read).to eq("scenario one\n")
+    end
+  end
+end
