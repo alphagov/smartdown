e0eed30f0ee55f8d32b9e247805c12f8000249d7
David Heath <david.heath@digital.cabinet-office.gov.uk>
2014-07-07 16:39:48 +0100

    Predicate evaluator for Equality and SetInclusion

--- lib/smartdown/engine/predicate_evaluator.rb
+++ lib/smartdown/engine/predicate_evaluator.rb
@@ -2,6 +2,19 @@ module Smartdown
   class Engine
     class PredicateEvaluator
       def evaluate(predicate, state)
+        evaluator_for(predicate).call(state)
+      end
+
+    private
+      def evaluator_for(predicate)
+        case predicate
+        when Smartdown::Model::Predicate::Equality
+          ->(state) { state.get(predicate.varname) == predicate.expected_value }
+        when Smartdown::Model::Predicate::SetMembership
+          ->(state) { predicate.values.include?(state.get(predicate.varname)) }
+        else
+          raise "Unknown predicate type #{predicate.class}"
+        end
       end
     end
   end
