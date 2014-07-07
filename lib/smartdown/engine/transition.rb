ee238074d867922a8ed996132c76727f41072656
David Heath <david.heath@digital.cabinet-office.gov.uk>
2014-07-07 17:30:00 +0100

    Handle nested rule evaluation

--- lib/smartdown/engine/transition.rb
+++ lib/smartdown/engine/transition.rb
@@ -13,11 +13,7 @@ module Smartdown
       end
 
       def next_node
-        if first_matching_rule
-          first_matching_rule.outcome
-        else
-          raise Smartdown::IndeterminateNextNode
-        end
+        first_matching_rule(next_node_rules.rules).outcome
       end
 
       def next_state
@@ -35,10 +31,28 @@ module Smartdown
         node.elements.find { |e| e.is_a?(Smartdown::Model::NextNodeRules) } or raise Smartdown::IndeterminateNextNode
       end
 
-      def first_matching_rule
-        @first_matching_rule ||= next_node_rules.rules.find do |rule|
-          predicate_evaluator.evaluate(rule.predicate, state_with_input)
+      def first_matching_rule(rules)
+        catch(:match) do
+          throw_first_matching_rule_in(rules)
         end
+      end
+
+      def throw_first_matching_rule_in(rules)
+        rules.each do |rule|
+          case rule
+          when Smartdown::Model::Rule
+            if predicate_evaluator.evaluate(rule.predicate, state_with_input)
+              throw(:match, rule)
+            end
+          when Smartdown::Model::NestedRule
+            if predicate_evaluator.evaluate(rule.predicate, state_with_input)
+              throw_first_matching_rule_in(rule.children)
+            end
+          else
+            raise "Unknown rule type"
+          end
+        end
+        raise Smartdown::IndeterminateNextNode
       end
 
       def state_with_input
