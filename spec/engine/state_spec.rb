263d203d5c9b268e57e13931ed062b35a9cf4ec7
David Heath <david.heath@digital.cabinet-office.gov.uk>
2014-07-08 11:18:56 +0100

    Add support for evaluating lambdas in state
    A lambda in state could be a predicate or a function injected by a
    plugin. They will be lazily evaluated and memoised, which we can safely
    do because the state is immutable.

--- spec/engine/state_spec.rb
+++ spec/engine/state_spec.rb
@@ -48,6 +48,43 @@ describe Smartdown::Engine::State do
     end
   end
 
+  context "lambda values" do
+    let(:predicate) { double("predicate", call: true) }
+    subject(:state) {
+      described_class.new(current_node: :start_state, predicate?: predicate)
+    }
+
+    describe "#get" do
+      it "evaluates lambda with state" do
+        expect(predicate).to receive(:call).with(state).and_return(true)
+        expect(state.get(:predicate?)).to eq(true)
+      end
+
+      it "caches the result of evaluating the lambda" do
+        expect(predicate).to receive(:call).once
+        state.get(:predicate?)
+        state.get(:predicate?)
+      end
+    end
+
+    describe "#==" do
+      let(:l1) { ->(state) { true } }
+      let(:l2) { ->(state) { true } }
+
+      let(:state_with_l1a) { described_class.new(current_node: "red", pred: l1)}
+      let(:state_with_l1b) { described_class.new(current_node: "red", pred: l1)}
+      let(:state_with_l2) { described_class.new(current_node: "red", pred: l2)}
+
+      it "is equal if identical lambdas" do
+        expect(state_with_l1a).to eq(state_with_l1b)
+      end
+
+      it "is not equal if different lambdas" do
+        expect(state_with_l1a).not_to eq(state_with_l2)
+      end
+    end
+  end
+
   describe "#==" do
     let(:s1) { described_class.new(current_node: "red") }
     let(:s2) { described_class.new(current_node: "red") }
