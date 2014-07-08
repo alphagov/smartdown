4a7dc9043ac380f00fb4d8e28da39d67994a121f
David Heath <david.heath@digital.cabinet-office.gov.uk>
2014-07-08 15:38:25 +0100

    Engine implementation using Engine::Transition

--- spec/engine_spec.rb
+++ spec/engine_spec.rb
@@ -1,58 +1,81 @@
 require 'smartdown/engine'
+require 'smartdown/model'
 
-xdescribe Smartdown::Engine do
+describe Smartdown::Engine do
+  let(:flow) {
+    Smartdown::Model.build do
+      flow("check-uk-visa") do
+        node("check-uk-visa") do
+          heading("Check uk visa")
+          paragraph("This is the paragraph")
+          start_button("what_passport_do_you_have?")
+          next_node_rules do
+            rule do
+              named_predicate("otherwise")
+              outcome("what_passport_do_you_have?")
+            end
+          end
+        end
+
+        node("what_passport_do_you_have?") do
+          heading("What passport do you have?")
+          multiple_choice(
+            greek: "Greek",
+            british: "British",
+            usa: "USA"
+          )
+          next_node_rules do
+            rule do
+              named_predicate("eea_passport?")
+              outcome("outcome_no_visa_needed")
+            end
+          end
+        end
+      end
+    end
+  }
+
+  subject(:engine) { Smartdown::Engine.new(flow) }
+  let(:start_state) {
+    engine.default_start_state
+      .put(:eea_passport?, ->(state) {
+        %w{greek british}.include?(state.get(:what_passport_do_you_have?))
+      })
+      .put(:otherwise, true)
+  }
 
   describe "#process" do
-    it "processes the given list of responses using the next_node_rules" do
-      new_state = instance_double("Smartdown::Model::State")
-      expect(next_node_rules).to receive(:transition).with(subject.start_state, "yes").and_return(new_state)
-      expect(subject.process(["yes"])).to eq new_state
-    end
+    subject { engine.process(responses, start_state) }
 
-    it "processes the given list of responses using the question" do
-      subject.process(["maybe"])
-    end
-  end
+    context "start button response only" do
+      let(:responses) { %w{yes} }
 
-  describe "#transition" do
-    let(:current_node_name) { "a_or_b?" }
-    let(:state) { Smartdown::Model::State.new(current_node: current_node_name) }
-    let(:input) { "yes" }
-    subject { described_class.new(current_node_name) }
+      it "is on what_passport_do_you_have?" do
+        expect(subject.get(:current_node)).to eq("what_passport_do_you_have?")
+      end
 
-    describe "no next node defined" do
-      it "raises" do
-        expect { subject.transition(state, input) }.to raise_error(Smartdown::IndeterminateNextNode)
+      it "has recorded input" do
+        expect(subject.get("check-uk-visa")).to eq("yes")
       end
     end
 
-    describe "next node defined" do
-      before(:each) {
-        subject.next_node(:outcome_1)
-      }
+    context "greek passport" do
+      let(:responses) { %w{yes greek} }
 
-      it "returns a new state with current_node set to the new node" do
-        subject.next_node(:outcome_1)
-        new_state = subject.transition(state, input)
-        expect(new_state.get(:current_node)).to eq :outcome_1
+      it "is on what_passport_do_you_have?" do
+        expect(subject.get(:current_node)).to eq("outcome_no_visa_needed")
       end
 
-      it "stores the input in new state using the name of the node" do
-        subject.next_node(:outcome_1)
-        new_state = subject.transition(state, input)
-        expect(new_state.get(:a_or_b)).to eq input
+      it "has recorded input" do
+        expect(subject.get("what_passport_do_you_have?")).to eq("greek")
       end
+    end
 
-      it "stores path in new state" do
-        subject.next_node(:outcome_1)
-        new_state = subject.transition(state, input)
-        expect(new_state.get(:path)).to eq [state.get(:current_node)]
-      end
+    context "USA passport" do
+      let(:responses) { %w{yes usa} }
 
-      it "stores responses in new state" do
-        subject.next_node(:outcome_1)
-        new_state = subject.transition(state, input)
-        expect(new_state.get(:responses)).to eq [input]
+      it "raises IndeterminateNextNode error" do
+        expect { subject }.to raise_error(Smartdown::IndeterminateNextNode)
       end
     end
   end
