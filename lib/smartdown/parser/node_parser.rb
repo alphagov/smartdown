65a998ad3bd30ea96d8cf95b385d541e1297cdc6
David Heath <david.heath@digital.cabinet-office.gov.uk>
2014-07-31 16:21:35 +0100

    Ensure markdown_blocks always produces array
    this trick of using repeat(1,1) matches one repetition of a clause but
    causes the match data produced to always be an array containing one
    element. This avoids the wierd edge-case that when there is a single
    markdown block you would get the markdown block alone, but when there
    are multiple you would get an array. Instead you always get an array.

--- lib/smartdown/parser/node_parser.rb
+++ lib/smartdown/parser/node_parser.rb
@@ -20,7 +20,7 @@ module Smartdown
       }
 
       rule(:markdown_blocks) {
-        markdown_block >> (newline.repeat(1) >> markdown_block).repeat
+        markdown_block.repeat(1, 1) >> (newline.repeat(1) >> markdown_block).repeat
       }
 
       rule(:body) {
