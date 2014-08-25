979551edbf80cd9bac9ebfb73e5085b15bc2a845
Camille Baldock <camille@camillebaldock.co.uk>
2014-08-25 15:53:37 +0100

    Allow for multiple question types

--- lib/smartdown/api/question_page.rb
+++ lib/smartdown/api/question_page.rb
@@ -6,8 +6,10 @@ module Smartdown
       def questions
         elements.slice_before do |element|
           element.is_a? Smartdown::Model::Element::MarkdownHeading
-        end.each_with_index.map do |question_element_group, index|
-          Smartdown::Api::MultipleChoice.new(question_element_group)
+        end.map do |question_element_group|
+          if question_element_group.find{|element| element.is_a? Smartdown::Model::Element::Question::MultipleChoice}
+            Smartdown::Api::MultipleChoice.new(question_element_group)
+          end
         end
       end
     end
