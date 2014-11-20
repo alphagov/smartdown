d716e1edfd8eb9f57b3bf8d0c6184bf4281c7d84
Jacopo Scotti <jacopojackscotti@gmail.com>
2014-11-20 17:05:49 +0000

    Don't humanize strings Data partials pass back HTML which is getting humanized and breaking the casing in the HTML. This still allows Answer objects to be humanized but will stop data partials getting mangled. Slightly hacky fix, that should be refactored when looking at Answer objects more generally.

--- lib/smartdown/engine/interpolator.rb
+++ lib/smartdown/engine/interpolator.rb
@@ -41,7 +41,10 @@ module Smartdown
         def interpolate(text, state)
           text.to_s.gsub(/%{([^}]+)}/) do |_|
             term = resolve_term($1, state)
-            term.respond_to?(:humanize) ? term.humanize : term
+            if term.respond_to?(:humanize) and !term.is_a?(String)
+              term = term.humanize
+            end
+            term
           end
         end
 
