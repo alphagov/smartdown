e2ed67d3240154b3257169c645fc5c86c82ba502
Jack Bracewell <jack.bracewell@unboxedconsulting.com>
2014-11-28 16:34:58 +0000

    Negated predicate model

--- lib/smartdown/model/predicate/negated.rb
+++ lib/smartdown/model/predicate/negated.rb
@@ -2,6 +2,13 @@ module Smartdown
   module Model
     module Predicate
       Negated = Struct.new(:predicate) do
+        def evaluate(state)
+          !predicate.evaluate(state)
+        end
+
+        def humanize
+          "NOT #{predicate.humanize}"
+        end
       end
     end
   end
