06dd5d1244403e4d372adbddd0bfbdc21c6c316e
Jack Bracewell <jack.bracewell@unboxedconsulting.com>
2014-12-09 10:48:39 +0000

    Remove monkey patch on node transformer.
    And we were already passing in node_name as context
    to the transform apply, doh!

--- lib/smartdown/parser/node_interpreter.rb
+++ lib/smartdown/parser/node_interpreter.rb
@@ -7,20 +7,21 @@ require 'smartdown/parser/node_transform'
 module Smartdown
   module Parser
     class NodeInterpreter
-      attr_reader :name, :source, :reporter
+      attr_reader :name, :source, :reporter, :data_module
 
       def initialize(name, source, options = {})
         @name = name
         @source = source
-        data_module = options.fetch(:data_module, {})
+        @data_module = options.fetch(:data_module, {})
         @parser = options.fetch(:parser, Smartdown::Parser::NodeParser.new)
-        @transform = options.fetch(:transform, Smartdown::Parser::NodeTransform.new(data_module))
+        @transform = options.fetch(:transform, Smartdown::Parser::NodeTransform.new)
         @reporter = options.fetch(:reporter, Parslet::ErrorReporter::Deepest.new)
       end
 
       def interpret
         transform.apply(parser.parse(source, reporter: reporter),
-          node_name: name
+          node_name: name,
+          data_module: data_module,
         )
       end
 
