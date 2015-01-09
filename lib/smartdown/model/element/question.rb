47792d7298acfa5f0bba9cf04b71c2ed5486c76c
Rob McKinnon <rob.mckinnon@digital.justice.gov.uk>
2015-01-09 15:17:59 +0000

    Reuse generic question creation code.

--- lib/smartdown/model/element/question.rb
+++ lib/smartdown/model/element/question.rb
@@ -3,17 +3,26 @@ module Smartdown
     module Element
       module Question
 
-        def self.create_question_answer elements, response
+        def self.create_question_answer elements, response=nil
           constants.find do |symbol|
             question_type = const_get(symbol)
 
             if element = elements.find {|e| e.is_a?(question_type) }
-              question = Smartdown::Api.const_get(symbol)
-              answer = element.answer_type.new(response, element)
+              question = question_model(symbol)
+              answer = response ? element.answer_type.new(response, element) : nil
               return [question.new(elements), answer]
             end
           end
           return [nil, nil]
+        end
+
+        private
+
+        def self.question_model symbol
+          unless Smartdown::Api.const_defined?(symbol)
+            symbol = (symbol.to_s + 'Question').to_sym
+          end
+          Smartdown::Api.const_get(symbol)
         end
 
       end
